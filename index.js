04df11326b85d5f8a9e5cc5898582e8155bb9120
12th-ai <mihigofololive@gmail.com>
2025-01-20 09:23:15 +0200

    learn to use anytype, void

---
+++ index.js
@@ -0,0 +1,5 @@
+var Double = function (x) { return x * x; };
+var printMessage = function (message) {
+    console.log("".concat(message));
+};
+printMessage("hello");
---
+++ index.ts
@@ -0,0 +1,8 @@
+const Double = (x:number):number =>  x * x
+
+
+const printMessage = (message:string):void =>{
+  console.log(`${message}`)
+}
+
+printMessage("hello")
